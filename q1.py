c3ebb72a1073856f42c189957be4c272a453fde8
nkesikbas25 <nkesikbas25@ku.edu.tr>
2026-04-02 23:10:55 +0300

    ...

--- q1.py
+++ q1.py
@@ -1,5 +1,13 @@
 def longest_palindromic_substring(s):
-    """
-    Given a string find the longest palindromic substring
-    """
-    pass
+    longest_palindrome=""
+    if len(s)<2:
+        return ""
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            string=s[i:j+1]
+            if string==string[::-1]:
+                if len(string)>len(longest_palindrome):
+                    longest_palindrome=string
+    if len(longest_palindrome)<2:
+        return ""
+    return longest_palindrome
--- q3b.py
+++ q3b.py
@@ -1,5 +1,23 @@
 def add_binary(a, b):
-    '''
-    Given two strings perform binary addition and return the result as a string
-    '''
-    pass
+    a=a.replace('0b',"")[::-1]
+    b=b.replace('0b',"")[::-1]
+    x=0
+    y=0
+    for i in range(len(a)):
+        if a[i]=='1':
+            x+=2**i
+    for i in range(len(b)):
+        if b[i]=='1':
+            y+=2**i
+    tot=x+y
+    res="" 
+    while tot>0:
+        res+=str(tot%2)
+        tot//=2
+    return "0b"+res[::-1]
+        
+        
+        
+        
+            
+            
